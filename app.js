877cd1e88ef9cf9110f040633f07c433436e6e09
Trevor Smith <trevor.smith1667@gmail.com>
2021-02-22 02:37:19 -0800

    working on bugs

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+const db = require("./config/keys").mongoURI
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User')
+const bodyParser = require('body-parser')
+const passport = require('passport');
 
 
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://admin:F5WGJ5TFq84f78Xy@mern.lkgib.mongodb.net/mern-twitter?retryWrites=true&w=majority',
+    secretOrKey: 'secret'
+}
